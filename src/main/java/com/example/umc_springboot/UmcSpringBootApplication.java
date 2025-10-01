9f317e19041b0f327db07d222d37833f549b40d8
nahjjun <nahjjun1125@naver.com>
2025-10-01 22:29:16 +0900

    [Week04] 스프링 프로젝트 초기 설정

---
+++ src/main/java/com/example/umc_springboot/UmcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcSpringBootApplication.class, args);
+    }
+
+}
